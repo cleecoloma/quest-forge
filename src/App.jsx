9b5b4cc5e8969a8b857d04d721e8a5e00235fcd9
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-04 14:22:44 -0800

    added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+'use strict';
+
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      {' '}
+      <div id='link-container'>
+        <a
+          href='https://www.linkedin.com/in/chesterleecoloma/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <LinkedInIcon id='linkedin-icon' />
+        </a>
+        <a
+          href='https://github.com/cleecoloma'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GitHubIcon id='github-icon' />
+        </a>
+      </div>
+      <div id='footer-container'>
+        <p>
+          Designed and built by
+          <br />
+          <strong>
+            <span>Chester Lee</span> <span>Coloma</span>
+          </strong>
+          <br />
+          2023
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
