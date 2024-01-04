a2a20f87ae371dd3fa72bf078fff8a4454af83a7
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-04 11:24:29 -0800

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './components/Header';
 import Hero from './components/Hero';
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <Hero />
     </>
   );
