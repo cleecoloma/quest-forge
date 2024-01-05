f418986b1a954ef2baab2522f9b454ba8bfff655
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-04 16:02:31 -0800

    added Routes and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import Create from './components/Create'
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Hero />
-        <Footer />
-      </BrowserRouter>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={
+            <>
+              <Hero />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route exact path='/create' element={<Create />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import '../styles/Hero.css';
 
 function Hero() {
+  const navigate = useNavigate();
+
+  const handleCreateClick = () => {
+    navigate('/create');
+  };
   return (
-      <div id='hero-container'>
-        <div id='hero-motto'>
-          <h1>Embark on Legends, Craft Your Destiny</h1>
-          <h2>Where Every Adventure is Handcrafted!</h2>
-          <div id='button-container'>
-            <Button variant='contained' color='error' id='create-button'>
-              Create a Hero
-            </Button>
-          </div>
+    <div id='hero-container'>
+      <div id='hero-motto'>
+        <h1>Embark on Legends, Craft Your Destiny</h1>
+        <h2>Where Every Adventure is Handcrafted!</h2>
+        <div id='button-container'>
+          <Button
+            variant='contained'
+            color='error'
+            id='create-button'
+            onClick={handleCreateClick}
+          >
+            Create a Hero
+          </Button>
         </div>
-        <img id='hero-image' src='public/images/hero.png' alt='hero' />
       </div>
+      <img id='hero-image' src='public/images/hero.png' alt='hero' />
+    </div>
   );
 }
 
