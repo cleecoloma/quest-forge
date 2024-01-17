50b4bdf4dfe2032753646fed5b3367bfc1d1986e
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-16 21:47:06 -0800

    updated timeout to 3 secs when creating hero

--- src/context/User/index.jsx
+++ src/context/User/index.jsx
@@ -19,7 +19,7 @@ function UserProvider(props) {
     heroObj.id = 1;
     setTimeout(() => {
       navigate('/quest');
-    }, 2000);
+    }, 3000);
     try {
       const response = await axios.put(`${SERVER_URL}/create`, heroObj);
       const responseData = response.data;
